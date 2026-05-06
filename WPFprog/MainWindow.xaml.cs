2588748d4f584202ce105a6b1d9ad1ba85988349
Ba6ai <153833234+Ba6ai@users.noreply.github.com>
2026-05-07 01:03:32 +0500

    t
    дописать сложение координаты х с числом пользователя

--- WPFprog/MainWindow.xaml.cs
+++ WPFprog/MainWindow.xaml.cs
@@ -8,47 +8,35 @@ namespace WPFprog
     {
         private void btnPlus_Click(object sender, RoutedEventArgs e)
         {
-            double x = Enter(txtX1);
-            double y = Enter(txtY1);
+            double x = double.Parse(txtX1.Text);
+            double y = double.Parse(txtY1.Text);
 
             ex2.Point p = new ex2.Point(x, y);
             p++;
             txtX1.Text = p.X.ToString();
         }
-
         private void btnMinus_Click(object sender, RoutedEventArgs e)
         {
-            double x = 0;
-            double y = 0;
-            if (double.TryParse(txtX1.Text, out x) && double.TryParse(txtY1.Text, out y))
-            {
-                ex2.Point p = new ex2.Point(x, y);
-                p--;
-                txtX1.Text = p.X.ToString();
-            }
-            else
-            {
-                txtResult.Text = "Ошибка ввода: введите числа";
-            }
+            double x = Enter(txtX1);
+            double y = Enter(txtY1);
+
+            ex2.Point p = new ex2.Point(x, y);
+            p--;
+            txtX1.Text = p.X.ToString();
         }
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
-            if (double.TryParse(txtX1.Text, out double x1) && 
-                double.TryParse(txtY1.Text, out double y1) &&
-                double.TryParse(txtX2.Text, out double x2) && 
-                double.TryParse(txtY2.Text,out double y2))
-            {
-                ex2.Point p1 = new ex2.Point(x1, y1);
-                ex2.Point p2 = new ex2.Point(x2, y2);
+            double x1 = Enter(txtX1);
+            double y1 = Enter(txtY1);
+            double x2 = Enter(txtX2);
+            double y2 = Enter(txtY2);
 
-                double dist = p1 + p2;
+            ex2.Point p1 = new ex2.Point(x1, y1);
+            ex2.Point p2 = new ex2.Point(x2, y2);
 
-                txtResult.Text = dist.ToString();
-            }
-            else
-            {
-                txtResult.Text = "Ошибка: заполните все поля";
-            }
+            double dist = p1 + p2;
+
+            txtResult.Text = dist.ToString();
         }
         private void btnInfo_Click(object sender, RoutedEventArgs e)
         {
@@ -63,6 +51,16 @@ namespace WPFprog
             InfoX.Text = Xint.ToString();
             InfoY.Text = Yd.ToString();
         }
+        private void btnPlusAA_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Enter(txtX1);
+            txtX1a.Text = x.ToString();
+            double y = Enter(txtY1);
+
+            
+
+            txtInfo.Text = x.ToString();
+        }
         private double Enter(TextBox text)
         {
             double x = 0;
@@ -72,8 +70,8 @@ namespace WPFprog
             }
             else
             {
-                txtResult.Text = "Ошибка ввода: введите числа";
-                return -1;
+                txtResult.Text = "Ошибка ввода: все поля точек должны быть заполнены";
+                return 0;
             }
         }
     }
