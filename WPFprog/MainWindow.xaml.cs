5faf9824ab5a6d93318e437a1241dcdfb52f4424
Ba6ai <153833234+Ba6ai@users.noreply.github.com>
2026-05-06 18:00:58 +0500

    test

---
+++ WPFprog/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFprog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    private void Button_Click(object  sender, RoutedEventArgs e)
+    {
+        string text = textBox1.Text;
+        if (text != " ")
+        {
+            MessageBox.Show(text);
+        }
+    }
+    private void textBox1_TExtChanged(object sender, TextChangedEventArgs e)
+    {
+        string text = textBox1.Text;
+        if (text != "no")
+        {
+            MessageBox.Show("Зачем ты так?)");
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
