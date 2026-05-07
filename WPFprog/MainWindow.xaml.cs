347144770f521c7be9477d8482c403950f100553
Ba6ai <153833234+Ba6ai@users.noreply.github.com>
2026-05-07 14:26:14 +0500

    ready

--- WPFprog/MainWindow.xaml.cs
+++ WPFprog/MainWindow.xaml.cs
@@ -8,12 +8,13 @@ namespace WPFprog
     {
         private void btnPlus_Click(object sender, RoutedEventArgs e)
         {
-            double x = double.Parse(txtX1.Text);
-            double y = double.Parse(txtY1.Text);
+            double x = Enter(txtX1);
+            double y = Enter(txtY1);
 
             ex2.Point p = new ex2.Point(x, y);
             p++;
             txtX1.Text = p.X.ToString();
+            txtX1a.Text = p.X.ToString();
         }
         private void btnMinus_Click(object sender, RoutedEventArgs e)
         {
@@ -23,6 +24,7 @@ namespace WPFprog
             ex2.Point p = new ex2.Point(x, y);
             p--;
             txtX1.Text = p.X.ToString();
+            txtX1a.Text = p.X.ToString();
         }
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
@@ -37,6 +39,7 @@ namespace WPFprog
             double dist = p1 + p2;
 
             txtResult.Text = dist.ToString();
+            txtX1a.Text = p1.X.ToString();
         }
         private void btnInfo_Click(object sender, RoutedEventArgs e)
         {
@@ -50,16 +53,31 @@ namespace WPFprog
 
             InfoX.Text = Xint.ToString();
             InfoY.Text = Yd.ToString();
+            txtX1a.Text = p.X.ToString();
         }
         private void btnPlusAA_Click(object sender, RoutedEventArgs e)
         {
-            double x = Enter(txtX1);
-            txtX1a.Text = x.ToString();
-            double y = Enter(txtY1);
+            double x1 = Enter(txtX1);
+            double y1 = Enter(txtY1);
 
+            ex2.Point p1 = new ex2.Point(x1, y1);
+
+            double AA = Enter(txtForSum);
             
+            p1 = p1 + AA;
 
-            txtInfo.Text = x.ToString();
+            txtInfoSum.Text = p1.X.ToString();
+        }
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            txtForSum.Text = "";
+            txtInfoSum.Text = "";
+            txtResult.Text = "";
+            txtX1.Text = "";
+            txtY1.Text = "";
+            txtX2.Text = "";
+            txtY2.Text = "";
+            txtX1a.Text = "";
         }
         private double Enter(TextBox text)
         {
